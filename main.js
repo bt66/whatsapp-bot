a4d252a2137c8367a5dab4d0d5c6880871117baa
bt66 <bastian.bt66@gmail.com>
2023-04-24 04:27:41 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const { body, validationResult } = require('express-validator');
+const express = require('express')
+const cors = require('cors')
+const basicAuth = require('express-basic-auth')
+require('dotenv').config()
+
+const app = express()
+const port = 3000
+// init express
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors())
+app.use(basicAuth({
+    users: { 'bt66': 'bt66' }
+}))
+// init whatsapp client
+const client = new Client({
+    authStrategy: new LocalAuth({
+        clientId: "client-one"
+    }),
+});
+
+client.initialize();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('authenticated', (session) => {
+    console.log('WHATSAPP WEB => Authenticated');
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+
+
+app.post('/send_message',
+    body('phone_number').notEmpty(),
+    body('message').notEmpty(),
+    (req, res) => {
+        const result = validationResult(req);
+        if (result.isEmpty()) {
+            try{
+                const number = req.body.phone_number;
+                const text = req.body.message;
+                const chatId = number.substring(1) + "@c.us";
+                // send message
+                client.sendMessage(chatId, text);
+                return res.status(200).send(req.body);
+            }catch(err) {
+                console.log(err)
+                return res.status(500).send(err)
+            }
+        }
+        res.send({ errors: result.array() });
+    }
+)
+
+app.listen(port, () => {
+    console.log(`Whatsapp bot listen on ${port}`)
+})
+ 
